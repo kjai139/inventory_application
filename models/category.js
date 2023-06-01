91a70eb177924a639256aed9c1198d2f22e20b32
simon y <71997308+kjai139@users.noreply.github.com>
2023-05-31 22:41:29 -0400

    populated mongodb

--- models/category.js
+++ models/category.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema
 
 const CategorySchema = new Schema({
     name:{ type: String, required: true, maxLength: 100},
+    description: {type: String, required: true}
 })
 
 CategorySchema.virtual('url').get(function (){
